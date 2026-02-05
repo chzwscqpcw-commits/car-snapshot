97413f3cdee068718cc0dab636daec7c7e63bc85
Stephen Gaisford <stephengaisford@Mac-mini.local>
2026-02-05 12:23:11 +0000

    Fix canonical URLs to use www version

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots() {
         disallow: "/api/",
       },
     ],
-    sitemap: "https://freeplatecheck.co.uk/sitemap.xml",
+    sitemap: "https://www.freeplatecheck.co.uk/sitemap.xml",
   };
 }
